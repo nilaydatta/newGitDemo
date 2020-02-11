b5868cffa5eb7b3cb6d2e87058fadd53d00c9c9b
nilaydatta <nilay.datta@live.com>
2020-02-11 14:49:06 +0530

    new files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* Main Javascript file */
+
+function displayContent(content) {
+	var root = document.getElementById('root');
+	root.innerHTML = content;
+	return;
+}
+
+var content = 'some text';
+
+displayContent(content);
+
+
